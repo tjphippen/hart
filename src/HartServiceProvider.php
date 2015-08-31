4bc10a542af6a329a0405438473240e1c3d37d21
root <root@box1.shawk.io>
2015-08-31 15:20:38 -0400

    update

--- src/HartServiceProvider.php
+++ src/HartServiceProvider.php
@@ -4,14 +4,20 @@ use Illuminate\Support\ServiceProvider;
 
 class HartServiceProvider extends ServiceProvider
 {
-
+    /**
+     *  Bootstrap the application events.
+     */
     public function boot()
     {
+        // Publish config files
         $this->publishes([
             __DIR__.'/config/config.php' => config_path('hart.php'),
         ]);
     }
 
+    /**
+     * Register the service provider.
+     */
     public function register()
     {
         $this->app['hart'] = $this->app->share(function($app)
@@ -20,6 +26,7 @@ class HartServiceProvider extends ServiceProvider
             return new Hart($config);
         });
 
+        // Auto boot the facade.
         $this->app->booting(function()
         {
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
